93902a594ccdd67bcd1be1f64ece647a92916b47
Samir ALILI <samir.alili.lbc@axa.fr>
2022-01-31 21:11:51 +0100

    Initial Commit

---
+++ Shared/TestSwinjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwinjectApp.swift
+//  Shared
+//
+//  Created by Samir ALILI on 31/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestSwinjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
